a6ba0dd095fa2e1157760bf49a50aa5f23bba9eb
rolandsobczak <rolandsobczak@icloud.com>
2023-06-17 11:53:37 +0200

    feat: User timestamped

--- src/social_bridge/models.py
+++ src/social_bridge/models.py
@@ -1,12 +1,21 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, DateTime, func, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 from social_bridge.database import Base, engine
 
+
 Base.metadata.create_all(bind=engine)
 
 
-class User(Base):
+class TimestampedModel(Base):
+    __abstract__ = True
+
+    created_at = Column(DateTime, default=func.now(), nullable=False)
+    updated_at = Column(DateTime, default=func.now(), onupdate=func.now(), nullable=False)
+
+
+class User(TimestampedModel):
     __tablename__ = "users"
 
     id = Column(Integer, autoincrement=True, primary_key=True, index=True)
@@ -16,4 +25,11 @@ class User(Base):
     last_name = Column(String)
     disabled = Column(Boolean)
     profile_image = Column(String)
+
+
+# class NGO(TimestampedModel):
+#     __tablename__ = "ngo"
+
+
+
 
