c1e66e7a4ff069f6542eb7d4193479ea501400f2
rolandsobczak <rolandsobczak@icloud.com>
2023-06-17 12:16:01 +0200

    feat: Company model

--- src/social_bridge/models.py
+++ src/social_bridge/models.py
@@ -34,6 +34,16 @@ class NGOMembership(TimestampedModel):
     ngo = relationship("NGO", back_populates="ngo")
 
 
+class CompanyMembership(TimestampedModel):
+    __tablename__ = "company_memberships"
+
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("User", back_populates="memberships")
+    company_id = Column(Integer, ForeignKey("companies.id"))
+    company = relationship("Company", back_populates="company")
+
+
 class User(TimestampedModel):
     __tablename__ = "users"
 
@@ -45,7 +55,8 @@ class User(TimestampedModel):
     disabled = Column(Boolean)
     profile_image = Column(String)
     account_type = Column(Integer)
-    ngo = relationship("NGOMemberships", secondary=NGOMembership, back_populates="user")
+    ngo = relationship("NGOMembership", secondary=NGOMembership, back_populates="user")
+    company = relationship("CompanyMembership", secondary=CompanyMembership, back_populates="user")
 
 
 class NGO(TimestampedModel):
@@ -55,10 +66,18 @@ class NGO(TimestampedModel):
     name = Column(String)
     image = Column(String)
     description = Column(String)
-    members = relationship("NGOMemberships", secondary=NGOMembership, back_populates="ngo")
+    members = relationship("NGOMembership", secondary=NGOMembership, back_populates="ngo")
     tax_number = Column(String)
     regon = Column(String)
 
 
+class Company(TimestampedModel):
+    __tablename__ = "companies"
 
-
+    id = Column(Integer, autoincrement=True, primary_key=True, index=True)
+    name = Column(String)
+    image = Column(String)
+    description = Column(String)
+    members = relationship("CompanyMembership", secondary=CompanyMembership, back_populates="company")
+    tax_number = Column(String)
+    regon = Column(String)
